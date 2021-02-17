577b5d8efeaf23b3d7c1e1eda4c24cad3c3ddb86
bluethefoxofficial <bluethefoxoffical@gmail.com>
2021-02-17 11:13:05 +0000

    Add project files.

---
+++ pixcheck/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace pixcheck
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(689, 592);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 10;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(847, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            this.label1.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(905, 745);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ pixcheck/Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pixcheck
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Start();
+        }
+        int num;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            pictureBox1.Left = Cursor.Position.X;
+            pictureBox1.Top = Cursor.Position.Y;
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                pictureBox1.Width--;
+                pictureBox1.Width--;
+                pictureBox1.Width--;
+
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                pictureBox1.Width++;
+                pictureBox1.Width++;
+                pictureBox1.Width++;
+
+            }
+            if (e.KeyCode == Keys.Up)
+            {
+                pictureBox1.Height++;
+                pictureBox1.Height++;
+                pictureBox1.Height++;
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                pictureBox1.Height--;
+                pictureBox1.Height--;
+                pictureBox1.Height--;
+            }
+            if (e.KeyCode == Keys.R)
+            {
+                pictureBox1.BackColor = Color.Red;
+            }
+            if (e.KeyCode == Keys.G)
+            {
+                pictureBox1.BackColor = Color.Green;
+            }
+            if (e.KeyCode == Keys.Escape)
+            {
+                Application.Exit();
+            }
+            if (e.KeyCode == Keys.H)
+            {
+                MessageBox.Show("Help box for Pixcheck and fix\nLeft arrow - make box wider\nRight arrow - make box thinner\nUp arrow - make box taller\nDown arrow - make box smaller\nR - set box colour to red\nG-set box colour to green\nB - set box color to blue\nW - set box to white\nEsc - Exit application\nQ - attempt repair \nO - repair frame counter\nH - help box");
+            }
+            if (e.KeyCode == Keys.B)
+            {
+                pictureBox1.BackColor = Color.Blue;
+            }
+            if (e.KeyCode == Keys.W)
+            {
+                pictureBox1.BackColor = Color.White;
+            }
+            if (e.KeyCode == Keys.Q)
+            {
+                MessageBox.Show("REPAIR MODE THIS WILL CAUSE FLASHING LOOK AWAY AFTER HITTING OK YOU HAVE BEEN WARNED", "Warning!!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                timer2.Start();
+                timer1.Stop();
+                pictureBox1.Visible = false;
+                pictureBox1.Hide();
+                this.BackColor = Color.Red;
+            }
+            if (e.KeyCode == Keys.O)
+            {
+                if(pictureBox1.Visible == true)
+                {
+
+                }
+                else
+                {
+                    label1.Visible = true;
+
+
+                }
+            }
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+
+         
+            if (this.BackColor == Color.Red)
+            {
+                this.BackColor = Color.Green;
+            }
+            else if (this.BackColor == Color.Green)
+            {
+                this.BackColor = Color.Blue;
+            }
+            else if (this.BackColor == Color.Blue)
+            {
+                this.BackColor = Color.Red;
+            }
+            else
+            {
+                this.BackColor = Color.Magenta;
+            }
+            num++;
+            label1.Text = num.ToString();
+            if(num == 360000000)
+            {
+                timer2.Stop();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
